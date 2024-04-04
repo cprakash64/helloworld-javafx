b749c3ff2191c290f19a11d1f03a3d55d1076bac
cprakash64 <cprakash64@users.noreply.github.com>
2024-04-04 14:46:11 -0700

    initial Commit

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label helloWorldLabel = new Label("Hello, World!");
+        StackPane root = new StackPane();
+        root.getChildren().add(helloWorldLabel);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
